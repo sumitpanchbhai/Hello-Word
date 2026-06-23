3fc6532f4000c3d835506d401cfa82abef22f26b
Sumit panchbhai <81416353+sumitpanchbhai@users.noreply.github.com>
2026-06-23 15:54:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,1 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def hello():
-    return jsonify(message="hello")
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+print("Hello World!)
